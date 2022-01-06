3aac89b1dea7ae9da31111eaff996c1da8ea9d66
Hady Walied <my.mepharo1230@gmail.com>
2022-01-06 00:12:40 +0000

    added the Json Adapter Annotations

--- app/src/main/java/com/github/hadywalied/airpurifier/domain/Entities.kt
+++ app/src/main/java/com/github/hadywalied/airpurifier/domain/Entities.kt
@@ -1,8 +1,11 @@
 package com.github.hadywalied.airpurifier.domain
 
+import com.squareup.moshi.JsonClass
+
 var CONFIG_URL = "192.168.100.1"
 var BASE_URL = "192.168.1.100"
 
+@JsonClass(generateAdapter = true)
 data class LightConfig(
     var strength: Int,
     var red: Int,
@@ -11,8 +14,10 @@ data class LightConfig(
     var mode: String
 )
 
+@JsonClass(generateAdapter = true)
 data class MotionSensorConfig(var state: Boolean)
 
+@JsonClass(generateAdapter = true)
 data class IntervalsConfig(
     var mode: String,
     var onTime: Int,
@@ -21,8 +26,11 @@ data class IntervalsConfig(
     var power: Int
 )
 
+@JsonClass(generateAdapter = true)
 data class OilData(var percentage: Int, var lifetime: Int)
 
+@JsonClass(generateAdapter = true)
 data class WifiConfig(var ssidName: String, var passwd: String)
 
+@JsonClass(generateAdapter = true)
 data class IpConfiguration(var ip: String)
