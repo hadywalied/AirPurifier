f87c4fea0c847083d1a3c17b4d75ff9a049e72c6
Hady Walied <my.mepharo1230@gmail.com>
2022-01-06 22:33:15 +0000

    added the OkHttp Helper

--- app/src/main/java/com/github/hadywalied/airpurifier/domain/NetworkUtils.kt
+++ app/src/main/java/com/github/hadywalied/airpurifier/domain/NetworkUtils.kt
@@ -1,2 +1,29 @@
 package com.github.hadywalied.airpurifier.domain
 
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import okio.IOException
+
+val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
+
+class OkHttpHelper {
+    var client: OkHttpClient = OkHttpClient()
+
+    fun post(url: String, json: String): Call {
+        val request = Request.Builder()
+            .url(url)
+            .post(json.toRequestBody(JSON))
+            .build()
+
+        return client.newCall(request)
+    }
+
+    fun get(url: String): Call {
+        val request = Request.Builder()
+            .url(url)
+            .get()
+            .build()
+        return client.newCall(request)
+    }
+}
